c9b9c55e859e0d9fd1cd81806832c45c71ffd985
xingkong576 <114883592@qq.com>
2026-08-18 10:06:07 +0800

    Purge repo: only contract pages remain

--- set_article.js
+++
@@ -1,9 +0,0 @@
-﻿const content = $escaped;
-const pm = document.querySelector('.ProseMirror');
-if (pm) {
-  pm.innerText = content;
-  pm.dispatchEvent(new Event('input', { bubbles: true }));
-  console.log('Content set successfully');
-} else {
-  console.log('ProseMirror not found');
-}
